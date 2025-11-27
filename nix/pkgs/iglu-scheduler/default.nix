7985e1be5389734478fd88a0e572c1b9dcdaa3d2
Svenum <s.ziegler@holypenguin.net>
2025-11-27 17:04:46 +0100

    fix formatting

--- nix/pkgs/iglu-scheduler/default.nix
+++ nix/pkgs/iglu-scheduler/default.nix
@@ -8,15 +8,15 @@ bun2nix.writeBunApplication {
 
   src = ../../..;
 
-  bunDeps = bun2nix.fetchBunDeps {                                   
-    bunNix = ./bun.nix;                                   
-  };                                   
-                                    
-  nativeBuildInputs = [                                   
-    deadnix                                   
-    nixpkgs-fmt                                   
-  ];                                   
-                                   
+  bunDeps = bun2nix.fetchBunDeps {
+    bunNix = ./bun.nix;
+  };
+
+  nativeBuildInputs = [
+    deadnix
+    nixpkgs-fmt
+  ];
+
   dontUseBunBuild = true;
 
   startScript = "bun run start";
