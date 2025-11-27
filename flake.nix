78ab7dd646466d006c9ce0259f0d123f7099f8ea
Svenum <s.ziegler@holypenguin.net>
2025-11-27 16:56:14 +0100

    update build flow

--- flake.nix
+++ flake.nix
@@ -4,10 +4,14 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus?ref=afcb15b845e74ac5e998358709b2b5fe42a948d1";
     iglu-flake.url = "github:iglu-sh/flake?ref=d9ca6b5d77b33ce20a0906b7c1491b792777bab5";
+    bun2nix = {
+      url = "github:nix-community/bun2nix?ref=2.0.3";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   # deadnix: skip
-  outputs = inputs@{ self, nixpkgs, utils, iglu-flake }:
+  outputs = inputs@{ self, nixpkgs, utils, bun2nix, iglu-flake }:
     utils.lib.mkFlake {
       inherit self inputs;
 
@@ -20,8 +24,8 @@
       overlay = import ./nix/pkgs;
 
       sharedOverlays = [
+        inputs.bun2nix.overlays.default
         inputs.iglu-flake.overlays.pkgs
-        inputs.iglu-flake.overlays.lib
         self.overlay
       ];
 
@@ -36,6 +40,8 @@
               wget
               cachix
               bun
+              nixpkgs-fmt
+              deadnix
               iglu.flakecheck
             ];
             shellHook = ''
